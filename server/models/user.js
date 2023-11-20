e348741e32d79741952e0c277ffd3fe9cdf83956
Ashish Thomas <ashishthomas2202@gmail.com>
2023-11-20 12:49:47 -0500

    created passport local strategy, serializer, deserializer

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema(
   {
@@ -22,6 +23,10 @@ const userSchema = new Schema(
       required: true,
       unique: true,
     },
+    salt: {
+      type: String,
+      required: true,
+    },
     hashedPassword: {
       type: String,
       required: true,
@@ -30,9 +35,38 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.virtual("fullName").get(() => {
+userSchema.virtual("fullname").get(() => {
   return `${this.firstName} ${this.lastName}`;
 });
+
+userSchema.virtual("password").set((password) => {
+  this._password = password;
+});
+
+userSchema.pre("save", async (next) => {
+  if (!this._password) {
+    return next();
+  }
+
+  try {
+    this.salt = await bcrypt.genSalt(10);
+    this.hashedPassword = await this.encryptPassword(this._password, this.salt);
+  } catch (error) {
+    return next(error);
+  }
+
+  next();
+});
+
+userSchema.methods = {
+  authenticate: async (password) => {
+    this._password = this.encryptPassword(password, this.salt);
+    return await bcrypt.compare(this._password, this.hashedPassword);
+  },
+  encryptPassword: async (password, salt) => {
+    return await bcrypt.hash(password, salt);
+  },
+};
 
 const User = mongoose.model("User", userSchema);
 
---
+++ server/utils/passport.js
@@ -0,0 +1,38 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+
+passport.use(
+  new localStrategy(async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return done(null, false, { message: "Email and Password not found." });
+      }
+
+      const isMatch = user.authenticate(password);
+      if (!isMatch) {
+        return done(null, false, {
+          message: "Email and Password do not match",
+        });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
